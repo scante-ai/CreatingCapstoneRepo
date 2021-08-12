6a864aed9ba473de6477183fa086d62625b960c6
Lin Zhang <linzhang294@gmail.com>
2021-08-12 03:00:59 -0400

    committing

--- NoteAppCoreData/NoteAppCoreData/Controllers/SingleViewVC.swift
+++ NoteAppCoreData/NoteAppCoreData/Controllers/SingleViewVC.swift
@@ -6,20 +6,19 @@
 //
 
 import UIKit
+import CoreData
 
 //var noteList = [Note]()
-
-//Class SingleNote: UIViewController {
-//    @IBOutlet weak var Description1: UILabel!
-//    @IBOutlet weak var Title1: UILabel!
-//    @IBOutlet weak var Date1: UILabel!
-//    @IBOutlet weak var Wavelength: UILabel!
-//}
 
 
 
 
 class SingleViewVC: UIViewController {
+    
+    @IBOutlet weak var Description1: UILabel!
+    @IBOutlet weak var Title1: UILabel!
+    @IBOutlet weak var Date1: UILabel!
+    @IBOutlet weak var Wavelength: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
